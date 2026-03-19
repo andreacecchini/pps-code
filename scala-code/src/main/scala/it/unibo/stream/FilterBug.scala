0c21ca47c5fe239c996585c519416f932e71b33f
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-03-19 13:00:57 +0100

    fix: typo

--- scala-code/src/main/scala/it/unibo/stream/FilterBug.scala
+++ scala-code/src/main/scala/it/unibo/stream/FilterBug.scala
@@ -15,7 +15,7 @@ package it.unibo.stream
   // The problem:
   val s1 = cons(x1, cons(x2, empty()))
   println("before filtering...")
-  val filtered = s1.filter(_ == 2) // `x1` and `x2` will not be computed!
+  val filtered = s1.filter(_ == 2) // `x1` and `x2` will be computed!
   println("after filtering...")
   print(filtered.toList)
   // Even worse:

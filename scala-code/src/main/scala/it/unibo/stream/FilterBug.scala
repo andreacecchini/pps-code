d37bf9cb0d322e7ebaa10c4a726e798f8826327d
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-03-19 13:02:31 +0100

    doc: adding comments

--- scala-code/src/main/scala/it/unibo/stream/FilterBug.scala
+++ scala-code/src/main/scala/it/unibo/stream/FilterBug.scala
@@ -17,7 +17,7 @@ package it.unibo.stream
   println("before filtering...")
   val filtered = s1.filter(_ == 2) // `x1` and `x2` will be computed!
   println("after filtering...")
-  print(filtered.toList)
+  print(filtered.toList) // `x1` and `x2` should be computed here!
   // Even worse:
   val naturals = iterate(0)(_ + 1)
-  naturals.filter(_ => false) // Loops!
+  // naturals.filter(_ => false) // Loops!

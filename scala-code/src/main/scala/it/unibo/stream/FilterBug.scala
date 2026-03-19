60b82ee30f4b340d89dd71c52d6ea568ef6a0ca0
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-03-19 13:19:28 +0100

    refactor: uncomment last filter

--- scala-code/src/main/scala/it/unibo/stream/FilterBug.scala
+++ scala-code/src/main/scala/it/unibo/stream/FilterBug.scala
@@ -12,12 +12,12 @@ package it.unibo.stream
     println("computing x2...")
     2
   }
-  // The problem:
+  // A problem:
   val s1: Stream[Int] = cons(x1, cons(x2, empty()))
   println("before filtering...")
   val filtered = s1.filter(_ == 2) // `x1` and `x2` will be computed!
   println("after filtering...")
-  print(filtered.toList) // `x1` and `x2` should be computed here!
-  // Even worse:
+  print(filtered.toList) // computation should start here!
+  // The problem:
   val naturals: Stream[Int] = iterate(0)(_ + 1)
-  // naturals.filter(_ => false) // Loops!
+  naturals.filter(_ => false) // Loops!

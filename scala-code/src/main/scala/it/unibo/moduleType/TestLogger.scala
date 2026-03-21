0fe08488fe50b487ef8b165146f2412d0a1efd3c
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-03-21 13:36:50 +0100

    refactor: importing given

--- scala-code/src/main/scala/it/unibo/moduleType/TestLogger.scala
+++ scala-code/src/main/scala/it/unibo/moduleType/TestLogger.scala
@@ -2,7 +2,7 @@ package it.unibo.moduleType
 
 
 @main def testLoggers(): Unit =
-  import LoggerModules.*
+  import LoggerModules.{*, given}
 
   def loggedCall[A, B](name: String, f: A => B, a: A)(using LoggerModule: LoggerModuleType): B =
     import LoggerModule.*
@@ -10,8 +10,10 @@ package it.unibo.moduleType
     f(a)
 
   // Injecting module implementation
-  // println(loggedCall("sin", math.sin, 0.5)(using BasicLoggerModule))
-  // println(loggedCall("sin", math.sin, 0.5)(using EmptyLoggerModule))
-  // Importing context
+  println:
+    loggedCall("sin", math.sin, 0.5)(using BasicLoggerModule)
+  println:
+    loggedCall("sin", math.sin, 0.5)(using EmptyLoggerModule)
+  // Imported context
   println:
     loggedCall("sin", math.sin, 0.5)

4a0707be111cab74dc990d00079eef6077d3cb71
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-03-22 12:11:04 +0100

    feat: overriding

--- scala-code/src/test/scala/it/unibo/moduleType/MapStoreTest.scala
+++ scala-code/src/test/scala/it/unibo/moduleType/MapStoreTest.scala
@@ -7,25 +7,32 @@ import org.junit.jupiter.api.Assertions.*
 class MapStoreTest:
 
   import KVStoreModule.*
+
   val storeModule: KVStore = MapStore
   import storeModule.*
 
   val empty: Store[String, Int] = emptyStore()
+  val k1 = "i1"
 
   @Test def testEmptyStore(): Unit =
     assertTrue:
       empty.isEmpty
 
+
   @Test def testAdd(): Unit =
-    val s1 = empty.put("i1", 1)
+    val s1 = empty.put(k1, 1)
     assertFalse:
       s1.isEmpty
-    assertEquals(Some(1), s1.get("i1"))
+    assertEquals(Some(1), s1.get(k1))
 
   @Test def testRemove(): Unit =
-    val s1 = empty.put("i1", 2)
-    val s2 = s1.remove("i1")
+    val s1 = empty.put(k1, 1)
+    val s2 = s1.remove(k1)
     assertTrue:
       s2.isEmpty
-    assertEquals(None, s2.get("i1"))
+    assertEquals(None, s2.get(k1))
 
+  @Test def testOverride(): Unit =
+    val s1 = empty.put(k1, 1)
+    val s2 = s1.put(k1, 2)
+    assertEquals(Some(2), s2.get(k1))

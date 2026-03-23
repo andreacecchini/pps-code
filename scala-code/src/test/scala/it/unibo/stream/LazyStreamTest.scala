0f225229e201f172b3f975a174627c75b7eaa36b
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-03-23 09:27:37 +0100

    feat: test for lazyness

--- scala-code/src/test/scala/it/unibo/stream/LazyStreamTest.scala
+++ scala-code/src/test/scala/it/unibo/stream/LazyStreamTest.scala
@@ -1,3 +1,19 @@
 package it.unibo.stream
 
-class LazyStreamTest extends StreamTest(using LazyStream)
+import Streams.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+
+val streamImpl: StreamADT = LazyStream
+given StreamADT = streamImpl
+
+class LazyStreamTest extends StreamTest:
+  import streamImpl.*
+
+  @Test def testLazyFilter(): Unit =
+    var c1 = 0
+    var c2 = 0
+    val str = cons({c1 = c1 + 1; 1}, cons({c2 = c2 + 1; 2}, empty()))
+    str.filter(_ => false)
+    assertEquals(0, c1)
+    assertEquals(0, c2)

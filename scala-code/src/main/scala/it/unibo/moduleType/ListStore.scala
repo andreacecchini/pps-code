d7bee5fd8129920d254c6b58f7e5661ba053a3cc
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-04-05 12:24:26 +0200

    refactor: using named tuple

--- scala-code/src/main/scala/it/unibo/moduleType/ListStore.scala
+++ scala-code/src/main/scala/it/unibo/moduleType/ListStore.scala
@@ -2,13 +2,13 @@ package it.unibo.moduleType
 
 
 private[moduleType] object ListStore extends KVStore:
-  opaque type Store[K, V] = List[(K, V)]
+  opaque type Store[K, V] = List[(key: K, value: V)]
 
   def emptyStore[K, V](): Store[K, V] = List.empty
 
   extension [K, V](s: Store[K, V])
     def isEmpty: Boolean = s.isEmpty
     def put(k: K, v: V): Store[K, V] = (k, v) :: s.remove(k)
-    def get(k: K): Option[V] = s.find(_._1 == k).map(_._2)
-    def remove(k: K): Store[K, V] = s.filterNot(_._1 == k)
+    def get(k: K): Option[V] = s.find(_.key == k).map(_.value)
+    def remove(k: K): Store[K, V] = s.filterNot(_.key == k)
 
